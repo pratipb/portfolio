1f4b487e911a917128b13d0ae53d2dc8e25c0279
pratipb <44521330+pratipb@users.noreply.github.com>
2021-05-15 07:42:31 +0000

    Deploying to gh-pages from  @ 9e94d2018058f0ad56eaf4226b46bd812ce5d326 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3929627b05089eae6e8fe15259f73b5e.js"
+  "/portfolio/precache-manifest.baba136448ba04e661dc0d3a7909b2b8.js"
 );
 
 self.addEventListener('message', (event) => {
